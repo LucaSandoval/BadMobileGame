73e4651e01bfb8ba71371fbefab96cf9ae1e48d3
remyrouth <remyrouth@gmail.com>
2023-08-02 23:06:43 -0400

    Update MenuPlay.cs

--- BadMobileGame/Assets/Resources/MenuPlay.cs
+++ BadMobileGame/Assets/Resources/MenuPlay.cs
@@ -1,4 +1,5 @@
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuPlay : MonoBehaviour
@@ -8,5 +9,38 @@ public class MenuPlay : MonoBehaviour
     public void LoadSceneByName()
     {
         SceneManager.LoadScene(MainScene);
+    }
+
+
+
+    public List<GameObject> settingObjects;
+    public List<GameObject> MainMenuObjects;
+
+    public void StartSettings() {
+        ActivateObjects(settingObjects);
+        DeactivateObjects(MainMenuObjects);
+    }
+
+    public void EndSettings() {
+        DeactivateObjects(settingObjects);
+        ActivateObjects(MainMenuObjects);
+    }
+
+    // Method to activate the specified objects
+    public void ActivateObjects(List<GameObject> objs)
+    {
+        foreach (GameObject obj in objs)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    // Method to deactivate the specified objects
+    public void DeactivateObjects(List<GameObject> objs)
+    {
+        foreach (GameObject obj in objs)
+        {
+            obj.SetActive(false);
+        }
     }
 }
